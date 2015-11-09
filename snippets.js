eb176417c204145af53aadbe471aa359cdc96325
shunia.huang <shunia@qq.com>
2015-11-09 18:35:29 +0800

    initial commit

---
+++ snippets.js
@@ -0,0 +1,4 @@
+// get url params
+// 获取URL参数
+var qd = {};
+location.search.substr(1).split("&").forEach(function(item) {var s = item.split("="), k = s[0], v = s[1] && decodeURIComponent(s[1]); (k in qd) ? qd[k].push(v) : qd[k] = [v]});
