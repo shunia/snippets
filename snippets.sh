5bb9377a51891158920898fb34a2fb72c0c3faab
Shunia Huang <shunia@qq.com>
2016-01-13 10:54:26 +0800

    增加在bash里解析时间戳的snippets

--- snippets.sh
+++ snippets.sh
@@ -7,3 +7,6 @@ sudo apt-get install -y nodejs
 # 在yum下安装nodejs 4.x
 curl --silent --location https://rpm.nodesource.com/setup | bash -
 sudo yum -y install nodejs
+
+# 时间戳转日期
+date -d @{any_timestamp_in_miliseconds_after_1970}
